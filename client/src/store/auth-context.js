58ae39fc87f7a5bfab79d2bc1c66d0f2beeaa966
riyoneri <lionkaneza@gmail.com>
2023-01-23 18:33:26 +0200

    Persisting user authentication

--- client/src/store/auth-context.js
+++ client/src/store/auth-context.js
@@ -8,16 +8,19 @@ const AuthContext = React.createContext({
 })
 
 export const AuthContextProvider = props => {
-    const [token, setToken] = useState(null)
+    const authToken = localStorage.getItem('a_t')
+    const [token, setToken] = useState(authToken)
 
     const userLoggedIn = !!token;
 
     const loginHandler = token => {
         setToken(token)
+        localStorage.setItem('a_t', token)
     }
 
     const logoutHandler = () => {
         setToken(null)
+        localStorage.removeItem('a_t')
     }
 
     const contextValue = {
